9a6913e001f9abe00d47341ec7288202faea709e
trinhquochai <trinhquochai12@gmail.com>
2024-03-13 14:05:24 +0700

    add

---
+++ js/index.js
@@ -0,0 +1,45 @@
+/* 
+Tạo 1 mảng chứa danh sách sản phẩm 
+quản lý product
+*/
+let products = [
+    {
+        image: "./assets/images/image1.jpg",
+        name: "iphone15",
+        price:"50000",
+        id:1,
+        stock:20
+    },
+    {
+        image: "./assets/images/image2.jpg",
+        name: "iphone14",
+        price: "40000",
+        id: 2,
+        stock: 20
+    }, {
+        image: "./assets/images/image3.jpg",
+        name: "iphone13",
+        price: "30000",
+        id: 3,
+        stock: 20
+    }, {
+        image: "./assets/images/image4.jpg",
+        name: "iphone12",
+        price: "10000",
+        id: 4,
+        stock: 20
+    }, {
+        image: "./assets/images/image5.jpg",
+        name: "iphone8",
+        price: "25000",
+        id: 5,
+        stock: 20
+    }, {
+        image: "./assets/images/image6.jpg",
+        name: "iphone5",
+        price: "15000",
+        id: 6,
+        stock: 20
+    },
+
+]
