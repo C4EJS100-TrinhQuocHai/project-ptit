0fc70d8ab6ec1e2bb96dbc5c0dd9caec70d4ab8b
trinhquochai <trinhquochai12@gmail.com>
2024-03-13 15:34:19 +0700

    add

--- js/index.js
+++ js/index.js
@@ -1,45 +1,28 @@
-/* 
-Tạo 1 mảng chứa danh sách sản phẩm 
-quản lý product
-*/
-let products = [
-    {
-        image: "./assets/images/image1.jpg",
-        name: "iphone15",
-        price:"50000",
-        id:1,
-        stock:20
-    },
-    {
-        image: "./assets/images/image2.jpg",
-        name: "iphone14",
-        price: "40000",
-        id: 2,
-        stock: 20
-    }, {
-        image: "./assets/images/image3.jpg",
-        name: "iphone13",
-        price: "30000",
-        id: 3,
-        stock: 20
-    }, {
-        image: "./assets/images/image4.jpg",
-        name: "iphone12",
-        price: "10000",
-        id: 4,
-        stock: 20
-    }, {
-        image: "./assets/images/image5.jpg",
-        name: "iphone8",
-        price: "25000",
-        id: 5,
-        stock: 20
-    }, {
-        image: "./assets/images/image6.jpg",
-        name: "iphone5",
-        price: "15000",
-        id: 6,
-        stock: 20
-    },
 
-]
+// lấy dữ liệu về đi render
+let products = JSON.parse(localStorage.getItem("products"));
+console.log("111111", products);
+
+//  function render product
+function renderProduct() {
+    let element = "";
+    for (let i = 0; i < products.length; i++) {
+        element +=
+            `
+                    <div class="product__item">
+                        <div class="image">
+                            <img src="${products[i].image}" alt="">
+                        </div>
+                        <p>${products[i].name}</p>
+                        <div>
+                            <p>price:${products[i].price}</p>
+                            <p><button>mua</button></p>
+                        </div>
+                    </div>
+                `
+    }
+    
+    // console.log("1111111111",element);
+    document.getElementById("products").innerHTML = element
+}
+renderProduct();
